ca494ecdaeb177095b6f38b118ac061e7b5dead8
w1nkl34 <sevan98t@gmail.com>
2022-04-25 01:14:38 +0300

    zbzb

--- Assets/Scripts/Constants/PublicImages.cs
+++ Assets/Scripts/Constants/PublicImages.cs
@@ -15,9 +15,9 @@ public class PublicImages : MonoBehaviour
     {
         string persistentUrl = iconUrl.Replace("/","");
         UnityWebRequest www = new UnityWebRequest();
-        if(File.Exists("file:///" + Application.persistentDataPath  + "/" + persistentUrl +".png"))
+        if(File.Exists("file://" + Application.persistentDataPath  + "/" + persistentUrl +".png"))
         {
-            www = UnityWebRequestTexture.GetTexture("file:///" + Application.persistentDataPath  + "/" + persistentUrl +".png");
+            www = UnityWebRequestTexture.GetTexture("file://" + Application.persistentDataPath  + "/" + persistentUrl +".png");
             yield return www.SendWebRequest();
             if (www.result != UnityWebRequest.Result.Success) 
                     {
@@ -50,7 +50,7 @@ public class PublicImages : MonoBehaviour
                     }
                     else 
                     {
-                        File.WriteAllBytes("file:///" + Application.persistentDataPath  + "/" + persistentUrl +".png",((DownloadHandlerTexture)www.downloadHandler).data);
+                        File.WriteAllBytes("file://" + Application.persistentDataPath  + "/" + persistentUrl +".png",((DownloadHandlerTexture)www.downloadHandler).data);
                         Texture2D myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
                         Sprite mySprite = Sprite.Create(myTexture, new Rect(0.0f, 0.0f, myTexture.width, myTexture.height), new Vector2(0.5f, 0.5f), 100.0f);
                         sp.sprite = mySprite;
